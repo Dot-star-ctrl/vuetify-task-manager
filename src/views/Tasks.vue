3142a04593dafe297aedd7fb7e6c1b1c30a1233a
georgi.minchev <gminchev.nl@gmail.com>
2021-12-26 19:06:06 +0100

    firebase connection

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -13,35 +13,20 @@
     ></v-text-field>
 
     <v-divider></v-divider>
-    <v-list
-        class="pt-0"
-        flat
-    >
+    <v-list class="pt-0" flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item
-        @click="doneTask(task.id)"
-        :class="{'blue lighten-5' : task.done}"
-        >
+        <v-list-item @click="doneTask(task.id)" :class="{'blue lighten-5' : task.done}">
           <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox
-                  :input-value="task.done"
-                  color="primary"
-              ></v-checkbox>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title
-              :class="{'text-decoration-line-through' : task.done}"
-              >
-                {{task.title}}
-              </v-list-item-title>
+              <v-list-item-title :class="{'text-decoration-line-through' : task.done}">{{task.title}}</v-list-item-title>
             </v-list-item-content>
 
             <v-list-item-action>
-              <v-btn
-                @click.stop="deleteTask(task.id)"
-                icon>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
                 <v-icon color="primary lighten-1">mdi-delete</v-icon>
               </v-btn>
             </v-list-item-action>
@@ -55,6 +40,8 @@
 </template>
 
 <script>
+import { db } from '../firebase/db';
+import { collection } from "firebase/firestore"; 
 export default {
   name: 'Home',
   data() {
@@ -63,23 +50,42 @@ export default {
       tasks: []
     }
   },
+  mounted() {
+    this.getTasks();
+  },
   methods: {
-    addTask() {
-      let newTask = {
-        id: Date.now(),
-        title: this.newTaskTitle,
-        done: false
+    async getTasks() {
+      let snapshot = await db.collection("tasks").get();
+      let tasks = [];
+      snapshot.forEach(doc => {
+        let appData = doc.data();
+        appData.id = doc.id;
+        tasks.push(appData);
+      });
+
+      this.tasks = tasks;
+    },
+    async addTask() {
+      if(this.newTaskTitle) {
+          await db.collection('tasks').add({title: this.newTaskTitle, done: false});
       }
-      this.tasks.push(newTask)
-      this.newTaskTitle = ''
     },
-    doneTask(id) {
-      let task = this.tasks.filter(task =>task.id === id)[0]
-      task.done = !task.done
+     async addTask () {
+      if(this.newTaskTitle) {
+        await db.collection("tasks").add({
+          title: this.newTaskTitle,
+          done: false
+        })
+        this.newTaskTitle = '';
+        this.getTasks();
+      } else {
+        alert('You must enter task first')
+      }
     },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter(task => task.id !== id)
+    async deleteTask (id) {
+      await db.collection("tasks").doc(id).delete()
+      this.getTasks()
     },
-  }
+  },
 }
 </script>
