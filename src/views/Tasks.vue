6748c44900f9073e671fee19854975fc56f57bff
georgi.minchev <gminchev.nl@gmail.com>
2022-01-02 18:16:23 +0100

    no tasks fallback added

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,32 +1,41 @@
 <template>
   <div class="home">
     <v-text-field
-        v-model="newTaskTitle"
-        @click:append="addTask"
-        @keyup.enter="addTask"
-        class="pa-3"
-        outlined
-        label="Add Task"
-        append-icon="mdi-plus"
-        hide-details
-        clearable
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
     ></v-text-field>
 
     <v-divider></v-divider>
-    <v-list class="pt-0" flat>
+    <v-list 
+    v-if="tasks.length"
+    class="pt-0" 
+    flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item @click="doneTask(task.id)" :class="{'blue lighten-5' : task.done}">
+        <v-list-item
+          :class="{ 'blue lighten-5': task.done }"
+        >
+
           <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+              <v-checkbox :input-value="task.done" @click="doneTask(task.id)" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title :class="{'text-decoration-line-through' : task.done}">{{task.title}}</v-list-item-title>
-            </v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
+              </v-list-item-content>
 
             <v-list-item-action>
-              <v-btn @click.stop="deleteTask(task.id)" icon>
+              <v-btn @click="deleteTask(task.id)" icon>
                 <v-icon color="primary lighten-1">mdi-delete</v-icon>
               </v-btn>
             </v-list-item-action>
@@ -36,19 +45,32 @@
         <v-divider></v-divider>
       </div>
     </v-list>
+    <div
+    v-else
+    class="no-tasks"
+    >
+    <v-icon
+    size="100"
+    color="primary">
+    mdi-check
+    </v-icon>
+    <div class="text-h5 primary--text">No tasks</div>
+    </div>
   </div>
 </template>
 
 <script>
-import { db } from '../firebase/db';
-import { collection } from "firebase/firestore"; 
+import { db } from "../firebase/db";
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      newTaskTitle: '',
-      tasks: []
-    }
+      newTaskTitle: "",
+      tasks: [],
+      dialog: false,
+      selectedTask: '',
+      deteleTaskId: '',
+    };
   },
   mounted() {
     this.getTasks();
@@ -57,7 +79,7 @@ export default {
     async getTasks() {
       let snapshot = await db.collection("tasks").get();
       let tasks = [];
-      snapshot.forEach(doc => {
+      snapshot.forEach((doc) => {
         let appData = doc.data();
         appData.id = doc.id;
         tasks.push(appData);
@@ -66,26 +88,69 @@ export default {
       this.tasks = tasks;
     },
     async addTask() {
-      if(this.newTaskTitle) {
-          await db.collection('tasks').add({title: this.newTaskTitle, done: false});
-      }
-    },
-     async addTask () {
-      if(this.newTaskTitle) {
-        await db.collection("tasks").add({
-          title: this.newTaskTitle,
-          done: false
-        })
-        this.newTaskTitle = '';
-        this.getTasks();
+      if (this.newTaskTitle) {
+        await db
+          .collection("tasks")
+          .add({
+            title: this.newTaskTitle,
+            done: false,
+          })
+          .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            this.newTaskTitle = "";
+            this.getTasks();
+          })
+          .catch((error) => {
+            console.error("Error adding document: ", error);
+          });
       } else {
-        alert('You must enter task first')
+        alert("You must enter task first");
       }
     },
-    async deleteTask (id) {
-      await db.collection("tasks").doc(id).delete()
-      this.getTasks()
+    async deleteTask(id) {
+      console.log(id);
+      await db.collection("tasks").doc(id).delete().then(() => {
+          console.log("Document successfully deleted!");
+          this.getTasks();
+          this.dialog = false;
+      }).catch((error) => {
+          console.error("Error removing document: ", error);
+      });
+    
+    },
+    async doneTask(id) {
+      var taskRef = await db.collection("tasks").doc(id);
+
+      taskRef
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            taskRef
+              .update({
+                done: !doc.data().done,
+              })
+              .then(() => {
+                this.getTasks();
+                console.log("Document successfully updated!");
+              });
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error getting document:", error);
+        });
     },
   },
-}
+};
 </script>
+
+<style lang="sass">
+.no-tasks
+  position: absolute
+  left: 50%
+  top: 50%
+  transform: translate(-50%, -50%)
+  opacity: 0.5
+</style>
